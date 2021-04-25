eab9837752e830631e43480bbe2704223f987c35
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2021-04-25 21:45:35 +0100

    created

---
+++ contracts/IStakeMaker.sol
@@ -0,0 +1,14 @@
+// "SPDX-License-Identifier: UNLINCENSED"
+pragma solidity >0.7.0 <0.9.0;
+
+/**
+ * This interface is purely for creating an unregistered stake
+ */ 
+
+interface IStakeMaker {
+    
+    function createUnregisteredStake(address _owner, address _holder, uint256 _amount, string memory _currencyName, 
+                                        address _erc20ContractAddress, string memory _ruleSetName, address _ruleSetAddress, 
+                                        string memory _details, uint256 _startDate, uint256 _endDate)external returns (address _unregisteredStakeAddress);
+    
+}
