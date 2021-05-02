818bbaa6a8c448bbb5c1b05f20fa981ae9fa90d6
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2021-05-02 22:47:50 +0100

    Update StakeMaker.sol
    Updated again

--- contracts/StakeMaker.sol
+++ contracts/StakeMaker.sol
@@ -8,25 +8,49 @@ import "./support/Stake.sol";
 contract StakeMaker is IStakeMaker {
     
     address [] createdStakes; 
+    mapping(address=>address[]) stakesByCreator; 
 
-    function createUnregisteredStake(address _owner, address _holder, uint256 _amount, string memory _currencyName, 
-                                    address _erc20ContractAddress, string memory _ruleSetName, address _ruleSetAddress,
-                                    string memory _details,  uint256 _startDate, uint256 _endDate) override external returns (address _unregisteredStakeAddress){
-         Stake unregisteredStake = new Stake(address(this),   _owner, 
-                            _holder, _amount, 
-                            _currencyName,  _erc20ContractAddress, 
-                            _ruleSetName, _ruleSetAddress, 
-                             _details, "UNREGISTERED");
+    function createUnregisteredStake(address _owner,
+                                        address _holder, 
+                                        uint256 _amount, 
+                                        string memory _currencyName, 
+                                        address _erc20ContractAddress, 
+                                        string memory _ruleSetName, 
+                                        address _ruleSetAddress,
+                                        string memory _details,  
+                                        uint256 _startDate, 
+                                        uint256 _endDate) override external returns (address _unregisteredStakeAddress){
+       
+         Stake unregisteredStake = new Stake(address(this), 
+                                                _owner, 
+                                                _holder, 
+                                                _amount, 
+                                                _currencyName,  
+                                                _erc20ContractAddress, 
+                                                _ruleSetName, 
+                                                _ruleSetAddress, 
+                                                _details, 
+                                                "UNREGISTERED", 
+                                                address(0));
+                                                
         unregisteredStake.setStartDate( _startDate);
         unregisteredStake.setEndDate( _endDate);
         
         createdStakes.push(address(unregisteredStake));
         
+        address unregisterStakeAddress = address(unregisteredStake);
+        
+        stakesByCreator[msg.sender].push(unregisterStakeAddress);
+        
         return address(unregisteredStake);
      }
      
      
-     function getUnregisteredStakes () external view returns(address [] memory _createdStakes) {
+     function getunregisteredStakes () override external view returns (address [] memory _createdStakes) {
+         return stakesByCreator[msg.sender];
+     }
+     
+     function getAllUnregisteredStakes () override external view returns(address [] memory _createdStakes) {
          return createdStakes;
      }
          
