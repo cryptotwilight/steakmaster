2aa9628ee2c3463905e5c2e5b51f9bb0de0331cb
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2021-05-02 22:46:04 +0100

    Update IStakeMaker.sol
    Updated again

--- contracts/IStakeMaker.sol
+++ contracts/IStakeMaker.sol
@@ -10,5 +10,10 @@ interface IStakeMaker {
     function createUnregisteredStake(address _owner, address _holder, uint256 _amount, string memory _currencyName, 
                                         address _erc20ContractAddress, string memory _ruleSetName, address _ruleSetAddress, 
                                         string memory _details, uint256 _startDate, uint256 _endDate)external returns (address _unregisteredStakeAddress);
+                                        
+    function getunregisteredStakes () external view returns (address [] memory _createdStakes);
+    
+    
+    function getAllUnregisteredStakes () external view returns(address [] memory _createdStakes);
     
 }
